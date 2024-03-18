969b6da55226512564c06d9491129776fd32e668
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-18 15:00:55 +0700

    feat: add sonner component and add toaster on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "@/routes/App"
+import { Toaster } from "@/components/ui/sonner"
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>,
 )
