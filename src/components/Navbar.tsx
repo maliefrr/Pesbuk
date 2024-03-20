6601171b2e47d1e498b742052063da5745f4bfa9
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-20 09:57:20 +0700

    fix: fix navbar display

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
-import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
-import {
-    Avatar,
-    AvatarFallback,
-    AvatarImage,
-  } from "@/components/ui/avatar"
+// import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
+// import {
+//     Avatar,
+//     AvatarFallback,
+//     AvatarImage,
+//   } from "@/components/ui/avatar"
 import Logo from "@/assets/logo-pesbuk.png"
 
 
@@ -15,24 +15,11 @@ const Navbar = () => {
     <div className="sticky top-0 z-50 w-full bg-[#306BC7] backdrop-blur supports-[backdrop-filter]:bg-[#306BC7] px-5">
       <nav className="container flex min-h-14 max-w-screen-2xl items-center justify-between">
         <Link to="/" className="text-2xl"><img src={Logo} alt="" className="h-12"/></Link>
-        <a href="">
-            <DropdownMenu>
-                <DropdownMenuTrigger>
-                    <Avatar>
-                        <AvatarImage src=""/>
-                        <AvatarFallback>PS</AvatarFallback>
-                    </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuItem>
-                        <Link to="/login">Login</Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                    <Link to="/register">Register</Link>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
-        </a>
+        <Link to="/login" className="text-white">Login</Link>
+        {/* <Avatar>
+            <AvatarImage src=""/>
+            <AvatarFallback>Login</AvatarFallback>
+          </Avatar> */}
       </nav>
     </div>
   )
