e4163099e591e4fa0bb7c364fce4ae26b70d068e
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-20 10:40:28 +0700

    fix: fix login display

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,27 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import Logo from "@/assets/logo-pesbuk.png"
+// import { Label } from "@/components/ui/label"
 
 const Login = () => {
   return (
-    <div className="bg-[#7289DA]">
+    <div className="bg-[#306BC7] font-inter">
       <div className="flex items-center justify-center h-screen">
-        <Card className="p-5 w-[80%] shadow-md bg-opacity-30 backdrop-blur-xl bg-white border-none lg:w-[30%] md:w-[40%]">
+        <Card className="p-5 w-[60%] shadow-lg backdrop-blur-xl bg-white border-none lg:w-[30%] md:w-[40%]">
           <CardContent>
-            <CardHeader><h1 className="text-center text-2xl">Login</h1></CardHeader>
+            <CardHeader>
+              <div className="flex flex-col justify-center items-center gap-4">
+                <img src={Logo} alt="" className="w-24"/>
+                <h1 className="text-3xl">Login</h1>
+              </div>
+            </CardHeader>
             <CardDescription>
               <form action="">
                 <div className="mb-3">
-                  <Label htmlFor="email">Email</Label>
                   <Input placeholder="Email" id="email"/>
                 </div>
                 <div className="mb-3">
-                  <Label htmlFor="password">Password</Label>
                   <Input placeholder="Password" id="password" type="password"/>
                 </div>
                 <div className="text-center mt-3">
