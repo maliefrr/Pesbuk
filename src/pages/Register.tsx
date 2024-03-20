676ff634691ae05b0857506812144280d1226beb
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-20 10:50:49 +0700

    fix: fix register display

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+// import { Label } from "@/components/ui/label"
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
 import { cn } from "@/lib/utils"
@@ -11,39 +11,41 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import Logo from "@/assets/logo-pesbuk.png"
 import { useState } from "react"
 
 
 const Register = () => {
   const [date,setDate] = useState<Date>()
   return (
-    <div className="bg-[#7289DA]">
-      <div className="flex items-center justify-center">
-        <Card className="p-5 w-[80%] shadow-md bg-opacity-30 backdrop-blur-xl bg-white border-none lg:w-[30%] md:w-[40%]">
+    <div className="bg-[#306BC7] font-inter">
+      <div className="flex items-center justify-center h-screen">
+        <Card className="p-5 w-[60%] shadow-lg backdrop-blur-xl bg-white border-none lg:w-[30%] md:w-[40%]">
           <CardContent>
-            <CardHeader><h1 className="text-center text-2xl">Register</h1></CardHeader>
+            <CardHeader>
+            <div className="flex flex-col justify-center items-center gap-4">
+                <img src={Logo} alt="" className="w-24"/>
+                <h1 className="text-3xl">Register</h1>
+              </div>
+            </CardHeader>
             <CardDescription>
               <form action="">
                 <div className="mb-3">
-                  <Label htmlFor="fullName">Nama Lengkap</Label>
                   <Input placeholder="Full Name" id="fullName"/>
                 </div>
                 <div className="mb-3">
-                  <Label htmlFor="email">Email</Label>
                   <Input placeholder="Email" id="email"/>
                 </div>
                 <div className="mb-3">
-                  <Label htmlFor="tempatLahir">Tempat Lahir</Label>
                   <Input placeholder="Tempat Lahir" id="tempatLahir"/>
                 </div>
                 <div className="mb-3">
-                  <Label>Tanggal Lahir</Label>
                   <Popover>
                     <PopoverTrigger asChild>
                       <Button
                         variant={"outline"}
                         className={cn(
-                          "w-full justify-start text-left font-normal",
+                          "w-full justify-between flex-row-reverse text-left font-normal",
                           !date && "text-muted-foreground"
                         )}
                       >
@@ -62,11 +64,9 @@ const Register = () => {
                   </Popover>
                 </div>
                 <div className="mb-3">
-                  <Label htmlFor="password">Password</Label>
                   <Input placeholder="Password" id="password" type="password"/>
                 </div>
                 <div className="mb-3">
-                  <Label htmlFor="passwordConfirmation">Password</Label>
                   <Input placeholder="Password Confirmation" id="passwordConfirmation" type="password"/>
                 </div>
                 <div className="text-center mt-3">
