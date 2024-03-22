65fb0e4d2eb277ae034a2d220d1bc2662815f003
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-22 19:12:32 +0700

    fix: fix formatter

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,51 +1,27 @@
 export const checkProperty = (property: any) => {
-    if (property instanceof File) {
-<<<<<<< HEAD
-      if (property.size !== 0) return true;
-    }
-  
-    if (typeof property === "string") {
-      if (property !== "") return true;
-    }
-  
-    if (typeof property === "boolean") {
-      return true;
-    }
-  
-    if (typeof property === "number") {
-      return true;
-    }
-  
-    return false;
-  };
+  if (property instanceof File) {
+    if (property.size !== 0) return true;
+  }
 
-  export const valueFormatData = (value: any) => {
-    if (value instanceof File) {
-      return value as Blob;
-    }
-  
-    return String(value);
-  };
-=======
-        if (property.size !== 0) return true;
-    }
+  if (typeof property === "string") {
+    if (property !== "") return true;
+  }
 
-    if (typeof property === "string") {
-        if (property !== "") return true;
-    }
+  if (typeof property === "boolean") {
+    return true;
+  }
 
-    if (typeof property === "boolean") {
-        return true;
-    }
+  if (typeof property === "number") {
+    return true;
+  }
 
-    if (typeof property === "number") {
-        return true;
-    }
-
-    return false;
+  return false;
 };
-  
+
 export const valueFormatData = (value: any) => {
-    return String(value);
+  if (value instanceof File) {
+    return value as Blob;
+  }
+
+  return String(value);
 };
->>>>>>> f8d874cb7f19685a2e76a2ac0364943016b4dcd5
