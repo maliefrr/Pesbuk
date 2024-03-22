7cf91145ea34aafd8792e414199920bc2b5e6d95
Mohamad Alief Rizky Ramadhan <maliefrr14@gmail.com>
2024-03-22 12:58:37 +0700

    feat: implement react hook form on login pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,10 +2,35 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import Logo from "@/assets/logo-pesbuk.png"
-import { Link } from "react-router-dom"
-// import { Label } from "@/components/ui/label"
+import { Link, useNavigate } from "react-router-dom"
+import {zodResolver} from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { LoginSchema, loginSchema } from "@/utils/apis/auth/type"
+import { loginUser } from "@/utils/apis/auth/api"
+import { toast } from "sonner"
+import { Form } from "@/components/ui/form"
+import { CustomFormField } from "@/components/customFormField"
+
 
 const Login = () => {
+  const navigate = useNavigate()
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  })
+
+  const login = async (body: LoginSchema) => {
+    try {
+      const response = await loginUser(body)
+      toast(response.message)
+      navigate("/")
+    } catch (error) {
+      toast((error as Error).message)
+    }
+  }
   return (
     <div className="bg-[#306BC7] font-inter">
       <div className="flex items-center justify-center h-screen">
@@ -18,20 +43,48 @@ const Login = () => {
               </div>
             </CardHeader>
             <CardDescription>
-              <form action="">
-                <div className="mb-3">
-                  <Input placeholder="Email" id="email"/>
-                </div>
-                <div className="mb-3">
-                  <Input placeholder="Password" id="password" type="password"/>
-                </div>
-                <div className="text-center mt-3">
-                  <Button className="mt-3 bg-[#306BC7] hover:bg-[#3360aa] duration-500" type="submit">Login</Button>
-                </div>
-                <div className="text-center mt-3">
-                  <p>Don't have account yet? <Link to="/register" className="hover:text-[#306BC7] duration-500">Register</Link></p>
-                </div>
-              </form>
+              <Form {...form}>
+                <form action="" onSubmit={form.handleSubmit(login)}>
+                  <div className="mb-3">
+                    <CustomFormField
+                      control={form.control}
+                      name="email"
+                    >
+                      {(field) => (
+                        <Input 
+                          placeholder="Email" 
+                          type="email"
+                          disabled={form.formState.isSubmitting}
+                          aria-disabled={form.formState.isSubmitting}
+                          {...field}
+                        />
+                      )}
+                    </CustomFormField>
+                  </div>
+                  <div className="mb-3">
+                    <CustomFormField
+                      control={form.control}
+                      name="password"
+                    >
+                      {(field) => (
+                        <Input 
+                          placeholder="Password" 
+                          type="password"
+                          disabled={form.formState.isSubmitting}
+                          aria-disabled={form.formState.isSubmitting}
+                          {...field}
+                        />
+                      )}
+                    </CustomFormField>
+                  </div>
+                  <div className="text-center mt-3">
+                    <Button className="mt-3 bg-[#306BC7] hover:bg-[#3360aa] duration-500" type="submit">Login</Button>
+                  </div>
+                  <div className="text-center mt-3">
+                    <p>Don't have account yet? <Link to="/register" className="hover:text-[#306BC7] duration-500">Register</Link></p>
+                  </div>
+                </form>
+              </Form>
             </CardDescription>
           </CardContent>
         </Card>
